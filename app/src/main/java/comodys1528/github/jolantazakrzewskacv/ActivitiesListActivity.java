275bb3f5d12354f88a92ed1339a5d5b8b613b635
Jolanta Zakrzewska <jolanta.zakrzewska@orange.pl>
2016-10-09 17:13:29 +0200

    added activity information in activities

--- app/src/main/java/comodys1528/github/jolantazakrzewskacv/ActivitiesListActivity.java
+++ app/src/main/java/comodys1528/github/jolantazakrzewskacv/ActivitiesListActivity.java
@@ -2,14 +2,17 @@ package comodys1528.github.jolantazakrzewskacv;
 
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ActivitiesListActivity extends AppCompatActivity{
     @Override
@@ -20,17 +23,19 @@ public class ActivitiesListActivity extends AppCompatActivity{
         ListView activitiesList = (ListView) findViewById(R.id.activity_list);
 
         final Activities[] activities = { new Activities("Kot", "straszny słodziak"), new Activities("Pies", "hau hau ab"),
-                new Activities("Słoń", "beniamin blimsien"), new Activities("Kot", "straszny słodziak"), new Activities("Pies", "hau hau ab"),
-                new Activities("Słoń", "beniamin blimsien"), new Activities("Kot", "straszny słodziak"), new Activities("Pies", "hau hau ab"),
-                new Activities("Słoń", "beniamin blimsien"), new Activities("Kot", "straszny słodziak"), new Activities("Pies", "hau hau ab"),
-                new Activities("Słoń", "beniamin blimsien"), new Activities("Kot", "straszny słodziak"), new Activities("Pies", "hau hau ab"),
-                new Activities("Słoń", "beniamin blimsien"), new Activities("Kot", "straszny słodziak"), new Activities("Pies", "hau hau ab"),
-                new Activities("Słoń", "beniamin blimsien"), new Activities("Kot", "straszny słodziak"), new Activities("Pies", "hau hau ab"),
                 new Activities("Słoń", "beniamin blimsien") };
 
         ActivitiesAdapter adapter = new ActivitiesAdapter(activities);
         activitiesList.setAdapter(adapter);
 
+        activitiesList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), ActivitiesItemActivity.class);
+                intent.putExtra("position", position);
+                startActivity(intent);
+            }
+        });
     }
 
     private class Activities {
