132a74837f510aacb1c0885c233ae566d3112609
Jolanta Zakrzewska <jolanta.zakrzewska@orange.pl>
2016-10-10 22:37:17 +0200

    deleted checkBoxes

--- app/src/main/java/comodys1528/github/jolantazakrzewskacv/ActivitiesItemActivity.java
+++ app/src/main/java/comodys1528/github/jolantazakrzewskacv/ActivitiesItemActivity.java
@@ -16,23 +16,19 @@ public class ActivitiesItemActivity extends AppCompatActivity {
         TextView name = (TextView) findViewById(R.id.activity_name);
         TextView date = (TextView) findViewById(R.id.activity_date);
         TextView description = (TextView) findViewById(R.id.activity_description);
-        CheckBox isCertified = (CheckBox) findViewById(R.id.is_certified_checkBox);
 
         if (item == 0) {
             name.setText("koteu");
             date.setText("6.10.1996");
             description.setText("so cute");
-            isCertified.setChecked(true);
         } else if (item == 1) {
             name.setText("pieseu");
             date.setText("7.10.1996");
             description.setText("so cute");
-            isCertified.setChecked(false);
         } else if (item == 2) {
             name.setText("słonieu");
             date.setText("8.10.1996");
             description.setText("so cute");
-            isCertified.setChecked(true);
         }
 
 
