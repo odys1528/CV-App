c5a4b3dcaa96647107550c3233c90bf6fb49a4c1
Jolanta Zakrzewska <jolanta.zakrzewska@orange.pl>
2016-10-17 17:20:11 +0200

    having fun with pics

--- app/src/main/java/comodys1528/github/jolantazakrzewskacv/InterestsActivity.java
+++ app/src/main/java/comodys1528/github/jolantazakrzewskacv/InterestsActivity.java
@@ -13,14 +13,16 @@ public class InterestsActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interests);
 
+        /*
         ImageView image1 = (ImageView) findViewById(R.id.imageView1);
-        ImageView image2 = (ImageView) findViewById(R.id.imageView2);
-        ImageView image3 = (ImageView) findViewById(R.id.imageView3);
+        ImageView image2 = (ImageView) findViewById(R.id.cosplay_image);
+        ImageView image3 = (ImageView) findViewById(R.id.development_image);
         ImageView image4 = (ImageView) findViewById(R.id.imageView4);
 
         Picasso.with(this).load(R.drawable.cosplay).resize(100, 100).into(image1);
         Picasso.with(this).load(R.drawable.cosplay).resize(100, 100).into(image2);
         Picasso.with(this).load(R.drawable.cosplay).resize(100, 100).into(image3);
         Picasso.with(this).load(R.drawable.cosplay).resize(100, 100).into(image4);
+        */
     }
 }
