be2281514f2c63a743d64a7273f7dea66964aa66
Jolanta Zakrzewska <jolanta.zakrzewska@orange.pl>
2016-10-20 21:26:15 +0200

    onClick for every pic in InterestsActivity

--- app/src/main/java/comodys1528/github/jolantazakrzewskacv/InterestsActivity.java
+++ app/src/main/java/comodys1528/github/jolantazakrzewskacv/InterestsActivity.java
@@ -3,26 +3,50 @@ package comodys1528.github.jolantazakrzewskacv;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
-import com.squareup.picasso.Picasso;
-
-public class InterestsActivity extends AppCompatActivity{
+public class InterestsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interests);
 
-        /*
-        ImageView image1 = (ImageView) findViewById(R.id.imageView1);
-        ImageView image2 = (ImageView) findViewById(R.id.cosplay_image);
-        ImageView image3 = (ImageView) findViewById(R.id.development_image);
-        ImageView image4 = (ImageView) findViewById(R.id.imageView4);
+        ImageView fantasyImage = (ImageView) findViewById(R.id.fantasy_image);
+        ImageView cosplayImage = (ImageView) findViewById(R.id.cosplay_image);
+        ImageView developmentImage = (ImageView) findViewById(R.id.development_image);
+        ImageView historyImage = (ImageView) findViewById(R.id.history_image);
 
-        Picasso.with(this).load(R.drawable.cosplay).resize(100, 100).into(image1);
-        Picasso.with(this).load(R.drawable.cosplay).resize(100, 100).into(image2);
-        Picasso.with(this).load(R.drawable.cosplay).resize(100, 100).into(image3);
-        Picasso.with(this).load(R.drawable.cosplay).resize(100, 100).into(image4);
-        */
+        final TextView description = (TextView) findViewById(R.id.description);
+
+        fantasyImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                description.setText("here will appear text about FANTASY");
+            }
+        });
+
+        cosplayImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                description.setText("here will appear text about COSPLAY");
+            }
+        });
+
+        developmentImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                description.setText("here will appear text about SELF-DEVELOPMENT");
+            }
+        });
+
+        historyImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                description.setText("here will appear text about HISTORY");
+            }
+        });
+
     }
 }
