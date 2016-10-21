2a2a2ca0b6edb2c0d56a4bc2314b20711eb80474
Jolanta Zakrzewska <jolanta.zakrzewska@orange.pl>
2016-10-21 10:07:08 +0200

    Picasso stuff in Interests

--- app/src/main/java/comodys1528/github/jolantazakrzewskacv/InterestsActivity.java
+++ app/src/main/java/comodys1528/github/jolantazakrzewskacv/InterestsActivity.java
@@ -7,22 +7,25 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
 public class InterestsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interests);
 
-        ImageView fantasyImage = (ImageView) findViewById(R.id.fantasy_image);
-        ImageView cosplayImage = (ImageView) findViewById(R.id.cosplay_image);
-        ImageView developmentImage = (ImageView) findViewById(R.id.development_image);
-        ImageView historyImage = (ImageView) findViewById(R.id.history_image);
+        final ImageView fantasyImage = (ImageView) findViewById(R.id.fantasy_image);
+        final ImageView cosplayImage = (ImageView) findViewById(R.id.cosplay_image);
+        final ImageView developmentImage = (ImageView) findViewById(R.id.development_image);
+        final ImageView historyImage = (ImageView) findViewById(R.id.history_image);
 
         final TextView description = (TextView) findViewById(R.id.description);
 
         fantasyImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View arg0) {
+                Picasso.with(InterestsActivity.this).load(R.drawable.fantasy).into(fantasyImage);
                 description.setText("here will appear text about FANTASY");
             }
         });
@@ -30,6 +33,7 @@ public class InterestsActivity extends AppCompatActivity {
         cosplayImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View arg0) {
+                Picasso.with(InterestsActivity.this).load(R.drawable.cosplay).into(cosplayImage);
                 description.setText("here will appear text about COSPLAY");
             }
         });
@@ -37,6 +41,7 @@ public class InterestsActivity extends AppCompatActivity {
         developmentImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View arg0) {
+                Picasso.with(InterestsActivity.this).load(R.drawable.development).into(developmentImage);
                 description.setText("here will appear text about SELF-DEVELOPMENT");
             }
         });
@@ -44,6 +49,7 @@ public class InterestsActivity extends AppCompatActivity {
         historyImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View arg0) {
+                Picasso.with(InterestsActivity.this).load(R.drawable.history).into(historyImage);
                 description.setText("here will appear text about HISTORY");
             }
         });
